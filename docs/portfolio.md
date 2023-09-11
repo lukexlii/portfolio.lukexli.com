e8a5c748d3a3fdee2f4b4b05d9c31d9845e232d0
Luke Li <luke@lukexli.com>
2023-09-11 15:29:11 -0400

    docs: add gatorapps account to portfolio

---
+++ docs/portfolio.md
@@ -0,0 +1,35 @@
+---
+sidebar_position: 1
+---
+
+# Portfolio
+
+## GatorApps Account
+
+- Identity Provider (IdP) for GatorApps (in progress project, a collection of apps to add joy and convenience to campus life).
+- MERN stack, Material UI, AWS
+- Implemented centralized session management to allow seamless user auth across all internal apps
+- Sign in once at account.gatorapps.org to access all apps under *.gatorapps.org; sign out in any app signs user out everywhere
+
+<p float="left">
+  <img alt="Sign in page" src="https://portfolio-lukexli-com.s3.us-east-1.amazonaws.com/img/ga_account-sign_in.jpg" width="50%" />
+  <img alt="User authenticate with University Shibboleth IdP" src="https://portfolio-lukexli-com.s3.us-east-1.amazonaws.com/img/ga_account-auth_uf.jpg" width="50%" />
+  Authenticate with school-managed accounts through University Shibboleth
+</p>
+
+<p float="left">
+  <img alt="Account dashboard profile section" src="https://portfolio-lukexli-com.s3.us-east-1.amazonaws.com/img/ga_account-left_menu.jpg" width="50%" />
+  <img alt="Dialog" src="https://portfolio-lukexli-com.s3.us-east-1.amazonaws.com/img/ga_account-dialogue.jpg" width="50%" />
+  Dynamic and adaptive rendering
+</p>
+
+<p float="left">
+  <img alt="Account dashboard profile section" src="https://portfolio-lukexli-com.s3.us-east-1.amazonaws.com/img/ga_account-loading_skeleton.jpg" width="50%" />
+  <img alt="Dialog" src="https://portfolio-lukexli-com.s3.us-east-1.amazonaws.com/img/ga_account-page_error.jpg" width="50%" />
+  Handle transitions and errors gracefully
+</p>
+
+<p float="left">
+  <img alt="Account dashboard profile section" src="https://portfolio-lukexli-com.s3.us-east-1.amazonaws.com/img/ga_account-dialogue_error.jpg" width="50%" />
+  <img alt="Dialog" src="https://portfolio-lukexli-com.s3.us-east-1.amazonaws.com/img/ga_account-app_error.jpg" width="50%" /> 
+</p>
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Portfolio',
           },
           //{ to: '/blog', label: 'Blog', position: 'left' },
           {
@@ -93,57 +93,57 @@ const config = {
       footer: {
         style: 'dark',
         links: [
-          /* {
-             title: 'Docs',
-             items: [
-               {
-                 label: 'Tutorial',
-                 to: '/docs/intro',
-               },
-             ],
-           },
-           {
-             title: 'Community',
-             items: [
-               {
-                 label: 'Stack Overflow',
-                 href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-               },
-               {
-                 label: 'Discord',
-                 href: 'https://discordapp.com/invite/docusaurus',
-               },
-               {
-                 label: 'Twitter',
-                 href: 'https://twitter.com/docusaurus',
-               },
-             ],
-           },
-           {
-             title: 'More',
-             items: [
-               {
-                 label: 'Blog',
-                 to: '/blog',
-               },
-               {
-                 label: 'GitHub',
-                 href: 'https://github.com/facebook/docusaurus',
-               },
-             ],
-           },
-           */
-          {
-            title: 'Contact',
-            items: [
-              {
-                label: 'Email',
-                href: 'javascript:void alert(\'My email: \' + String.fromCharCode(119,101,98,45,99,111,110,116,97,99,116,64,108,117,107,101,120,108,105,46,99,111,109))"',
-              },
-            ],
-          },
+          /*
+                    {
+                       title: 'Docs',
+                       items: [
+                         {
+                           label: 'Tutorial',
+                           to: '/docs/intro',
+                         },
+                       ],
+                     },
+                     {
+                       title: 'Community',
+                       items: [
+                         {
+                           label: 'Stack Overflow',
+                           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                         },
+                         {
+                           label: 'Discord',
+                           href: 'https://discordapp.com/invite/docusaurus',
+                         },
+                         {
+                           label: 'Twitter',
+                           href: 'https://twitter.com/docusaurus',
+                         },
+                       ],
+                     },
+                     {
+                       title: 'More',
+                       items: [
+                         {
+                           label: 'Blog',
+                           to: '/blog',
+                         },
+                         {
+                           label: 'GitHub',
+                           href: 'https://github.com/facebook/docusaurus',
+                         },
+                       ],
+                     },
+                    {
+                      title: 'Contact',
+                      items: [
+                        {
+                          label: 'Email',
+                          href: 'javascript:void alert(\'My email: \' + String.fromCharCode(119,101,98,45,99,111,110,116,97,99,116,64,108,117,107,101,120,108,105,46,99,111,109))"',
+                        },
+                      ],
+                    },
+          */
         ],
-
         copyright: `Luke Li. Built with Docusaurus.`,
       },
       prism: {
