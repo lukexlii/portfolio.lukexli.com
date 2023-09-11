5f49d6920cfc86c53c9f1977e2a30a6f6491d3fb
Luke Li <luke@lukexli.com>
2023-09-11 00:26:25 -0400

    initial commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/lukeli/Desktop/GA/portfolio.lukexli.com/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/lukeli/Desktop/GA/portfolio.lukexli.com/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/lukeli/Desktop/GA/portfolio.lukexli.com/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/lukeli/Desktop/GA/portfolio.lukexli.com/src/css/custom.css'),
+];
